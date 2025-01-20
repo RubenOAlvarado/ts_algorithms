36b15113c9a2ecaf264c3cab294388418ba63f31
Ruben Alvarado <ruben.alvardo@plexusworldwide.com>
2025-01-20 12:00:56 -0600

    chore: spiral matrix

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-import { pyramid } from './pyramid-string-pattern';
-
-console.log(" --- Pyramid string patterns --- ");
-console.log("pyramid(1)");
-pyramid(1);
-console.log("pyramid(2)");
-pyramid(2);
-console.log("pyramid(3)");
-pyramid(3);
+console.log('Hello, world!');
