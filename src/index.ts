ee8cb203b206706a1360a3084f51b8d4ed335d01
Ruben Alvarado <ruben.alvardo@plexusworldwide.com>
2025-01-20 10:22:26 -0600

    chore: fizzbuzz

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-import { vowels, vowelsV2 } from './vowels';
+import { fizzBuzz } from './fizzbuzz';
 
-console.log(" --- Vowels in a string --- ");
-console.log(`vowels('Hi there!') --> ${vowels('Hi there!')}`);
-console.log(`vowels('Why do you ask?') --> ${vowels('Why do you ask?')}`);
-console.log(`vowels('Why?') --> ${vowels('Why?')}`);
-
-console.log(" --- Vowels in a string V2 --- ");
-console.log(`vowelsV2('Hi there!') --> ${vowelsV2('Hi there!')}`);
-console.log(`vowelsV2('Why do you ask?') --> ${vowelsV2('Why do you ask?')}`);
-console.log(`vowelsV2('Why?') --> ${vowelsV2('Why?')}`);
+console.log(" --- FizzBuzz --- ");
+console.log("fizzBuzz(5) --> ");
+fizzBuzz(5);
+console.log("fizzBuzz(15) --> ");
+fizzBuzz(15);
+console.log("fizzBuzz(100) --> ");
+fizzBuzz(100);
